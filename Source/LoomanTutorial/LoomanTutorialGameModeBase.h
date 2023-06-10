a5e80c8a77ea20e1d5de473bd9bb2dc900dd1e47
jcavers <136112264+jcavers@users.noreply.github.com>
2023-06-09 17:25:37 -0700

    Added SCharacter & Setup Input
    - Added Character class.
    - Added MoveForward/Turn input

---
+++ Source/LoomanTutorial/LoomanTutorialGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LoomanTutorialGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LOOMANTUTORIAL_API ALoomanTutorialGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
